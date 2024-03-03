7b7942b3b5d773449cd7f80c34f95468d995cfb2
Ali Jaafar <ahj2106@gmail.com>
2024-03-03 14:57:41 +0200

    Fix prints not showing in the logs

--- utils/cohere.py
+++ utils/cohere.py
@@ -9,7 +9,7 @@ async def query_cohere(prompt):
         response = co.chat(message=prompt, model='command', temperature=0.9)
         words = response.text.split(' ')
     except Exception as e:
-        print(f'An error has occurred: {e}')
+        print(f'An error has occurred: {e}', flush=True)
         words = 'Oops, something went wrong!'.split(' ')
 
     for word in words:
