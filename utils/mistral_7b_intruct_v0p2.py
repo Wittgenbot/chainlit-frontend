93f606a7466514e290fa04ce9d1e5dfde3cce0e6
Ali Jaafar <ahj2106@gmail.com>
2024-03-11 15:30:00 +0200

    Fix the event loop issue when fetching or invoking a Modal function

--- utils/mistral_7b_intruct_v0p2.py
+++ utils/mistral_7b_intruct_v0p2.py
@@ -1,8 +1,13 @@
 import modal
+import asyncio, nest_asyncio
+
+try:
+    asyncio.get_event_loop()
+    nest_asyncio.apply()
+except RuntimeError:
+    pass
 
 async def query_mistral(prompt):
-    print('fetching the function')
     query_mistral_7b_instruct_v0p2 = modal.Function.lookup(app_name='philosophy-question-answerer', tag='query_mistral_7b_instruct_v0p2')
-    print('querying Mistral')
     response = query_mistral_7b_instruct_v0p2.remote(prompt)
     return response
