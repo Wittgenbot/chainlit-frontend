0788a2d2d859a95d5609a0260f96f3f58ce87a5e
Ali Jaafar <ahj2106@gmail.com>
2024-04-04 13:08:05 +0300

    Fix avatar bug and update readme
    Co-authored-by: Nadim Akkaoui <nadimakkaoui1@gmail.com>

--- utils/avatars.py
+++ utils/avatars.py
@@ -1,6 +1,6 @@
 from chainlit import Avatar
 
 async def collect_avatars():
-    await Avatar(name='Wittgenbot-FT', path='./public/WITT-FT_icon.png').send()
-    await Avatar(name='Wittgenbot-0.5K', path='./public/WITT-0.5K_icon.png').send()
-    await Avatar(name='Wittgenbot-1.5K', path='./public/WITT-1.5K_icon.png').send()
+    await Avatar(name='Wittgenbot-Fine-Tuned', path='./public/WITT-FT_icon.png').send()
+    await Avatar(name='Wittgenbot-512', path='./public/WITT-0.5K_icon.png').send()
+    await Avatar(name='Wittgenbot-1500', path='./public/WITT-1.5K_icon.png').send()
