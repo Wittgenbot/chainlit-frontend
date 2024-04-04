fc9e1836b13800c99b285a47ee77ad87581230a4
Nadim Akkaoui <nadimakkaoui1@gmail.com>
2024-04-04 09:58:20 +0300

    update readme to include FT model

--- utils/chat_profiles.py
+++ utils/chat_profiles.py
@@ -2,18 +2,18 @@ from chainlit import ChatProfile
 
 wittgenbot_ft_chat_profile = ChatProfile(
     name='WITT-FT',
-    markdown_description='Wittgenbot fine-tuned on a QA dataset.',
+    markdown_description='<center>Wittgenbot fine-tuned on a QA dataset.</center>',
     icon='/public/WITT-FT_icon.png',
 )
 
 wittgenbot_0p5k_chat_profile = ChatProfile(
     name='WITT-0.5K',
-    markdown_description='Wittgenbot with a context length of 512.',
+    markdown_description='<center>Wittgenbot using RAG with a context length of 512.</center>',
     icon='/public/WITT-0.5K_icon.png',
 )
 
 wittgenbot_1p5k_chat_profile = ChatProfile(
     name='WITT-1.5K',
-    markdown_description='Wittgenbot with a context length of 1500.',
+    markdown_description = '<center>Wittgenbot using RAG with a context length of 1500.</center>',
     icon='/public/WITT-1.5K_icon.png',
 )
